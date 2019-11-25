99b70e522a93cc4da561a5813289022866bf0894
Qillor <samcjones32@gmail.com>
2019-11-24 20:39:15 -0600

    Add in x,y co-ordinates for the brick rows

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -2,6 +2,12 @@ var c = document.getElementById("myCanvas");
 /** @type {CanvasRenderingContext2D} */
 var ctx = c.getContext("2d");
 ctx.beginPath();
-ctx.rect(45,45,45,45);
+for (x = 2; x < 600; x += 50) {
+    for (y = 2; y < 150; y += 50) {
+        ctx.rect(x,y,45,45);
+        ctx.fillStyle= "#89cff0";
+        ctx.fill();
+    }
+}
 ctx.closePath();
-
+  
