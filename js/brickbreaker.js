cf679a315dd02a01ee401b36173a631183faba45
Qillor <samcjones32@gmail.com>
2019-11-26 22:39:07 -0600

    having problems drawing the paddle.

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -1,16 +1,18 @@
 var c = document.getElementById("myCanvas");
 /** @type {CanvasRenderingContext2D} */
 var ctx = c.getContext("2d");
+var left = false;
+var right = false;
+var xaxis = 0;
 var ballx = 0;
-var bally = 150;
-var left = false
-var right = false
+var bally = 0;
+
 function brick() {
     ctx.beginPath();
     for (x = 2; x < 600; x += 50) {
         for (y = 2; y < 150; y += 50) {
             ctx.rect(x, y, 45, 45);
-            ctx.fillStyle = "#89cff0";
+            ctx.fillStyle = "#800080";
             ctx.fill();
         }
     }
@@ -26,37 +28,42 @@ function ball() {
 
 function paddle() {
     ctx.beginPath();
-    ctx.rect(275, 400, 60, 10)
-    ctx.stroke();
+    ctx.rect(xaxis, 400, 60, 10);
+    ctx.fillstyle = "#00ffff"
+    ctx.fill(); 
     ctx.closePath();
 }
 
 function components() {
-    ctx.clearRect(0, 0, c.clientWidth, c.height)
+    ctx.clearRect(0, 0, c.Width, c.Height);
     ball();
     brick();
     paddle();
-
-    ballx += 5;
-    bally += 5;
+  
+    if (right == true)
+        xaxis += 5;
+    if (left == true)
+        xaxis -= 5;
+    
 }
 
 
 function keyDown(event) {
     if (event.keyCode == 39)
-        alert("This makes the paddle move right");
+        right = true;
     if (event.keyCode == 37)
-        alert("bruh")
+        left = true;
 }
 
 function keyUp(event) {
+    if (event.keyCode == 39)
+        right = false;
+    if (event.keyCode == 37)
+        left = false;
 
 }
 
-
-
 document.addEventListener("keydown", keyDown, false);
-
 document.addEventListener("keyup", keyUp, false);
 
 setInterval(components, 20);
