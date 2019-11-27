5d702eab4cf30c163d75799dc52e70f58f8a2508
Qillor <samcjones32@gmail.com>
2019-11-26 20:53:51 -0600

    added keydown and keyup function, capturing the keys.

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -3,41 +3,60 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var ballx = 0;
 var bally = 150;
-
+var left = false
+var right = false
 function brick() {
     ctx.beginPath();
     for (x = 2; x < 600; x += 50) {
         for (y = 2; y < 150; y += 50) {
-            ctx.rect(x,y,45,45);
-            ctx.fillStyle= "#89cff0";
+            ctx.rect(x, y, 45, 45);
+            ctx.fillStyle = "#89cff0";
             ctx.fill();
         }
     }
-    ctx.closePath(); 
+    ctx.closePath();
 }
 
-function ball(){
+function ball() {
     ctx.beginPath();
     ctx.arc(ballx, bally, 25, 0, 2 * Math.PI);
     ctx.stroke();
     ctx.closePath();
 }
- 
+
 function paddle() {
     ctx.beginPath();
-    ctx.rect(275,400,60,10)
+    ctx.rect(275, 400, 60, 10)
     ctx.stroke();
     ctx.closePath();
-}    
+}
 
-function components(){
-    ctx.clearRect(0,0,c.clientWidth,c.height)
+function components() {
+    ctx.clearRect(0, 0, c.clientWidth, c.height)
     ball();
     brick();
     paddle();
 
-    ballx+=5;
-    bally+=5;
+    ballx += 5;
+    bally += 5;
 }
+
+
+function keyDown(event) {
+    if (event.keyCode == 39)
+        alert("This makes the paddle move right");
+    if (event.keyCode == 37)
+        alert("bruh")
+}
+
+function keyUp(event) {
+
+}
+
+
+
+document.addEventListener("keydown", keyDown, false);
+
+document.addEventListener("keyup", keyUp, false);
 
 setInterval(components, 20);
